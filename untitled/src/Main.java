0b133d042cdb702c14837a9f40af30d9d3a0ea23
s33128 <s33128@pjwstk.edu.pl>
2026-05-20 16:15:19 +0200

    zadanie2

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -14,5 +14,15 @@ public class Main {
         System.out.println("Valid: " + validator.isValid(form));
 
         System.out.println("Zadanie 2:");
+        ServiceOrder order = new ServiceOrder("Firma Alfa", 10, 120.0);
+        PriceCalculator calculator = new PriceCalculator();
+
+        PriceStrategy standard = o -> o.hours() * o.hourRate();
+        PriceStrategy discount = o -> o.hours() * o.hourRate() * 0.90;
+        PriceStrategy weekend = o -> o.hours() * o.hourRate() * 1.25;
+
+        System.out.println(calculator.calculate(order, standard));
+        System.out.println(calculator.calculate(order, discount));
+        System.out.println(calculator.calculate(order, weekend));
     }
 }
